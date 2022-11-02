38777845aef3d156ed9da89f884003473b68c161
jinhans <wlsgks846@gmail.com>
2022-11-02 15:12:15 +0900

    커스텀 Hooks 만들기

--- Info.js
+++ Info.js
@@ -1,35 +1,25 @@
-import { useState, useEffect } from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  useEffect(() => {
-    console.log("effect");
-    return () => {
-      console.log("unmount");
-    };
-  }, []);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
 
   return (
     <div>
       <div>
-        <input vlaue={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name='name' value={name} onChange={onChange} />
+        <input name='nickname' value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
           <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nickname}
+          <b>닉네임:</b>
+          {nickname}
         </div>
       </div>
     </div>
